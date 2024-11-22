e408f133947f4e00735a31cb673b0b2b23234db8
AndriiLendiel <lendiel9333@gmail.com>
2024-11-22 15:10:39 +0200

    l2

--- src/api/apiNews.js
+++ src/api/apiNews.js
@@ -1,9 +1,12 @@
 import axios from 'axios'
 
 
-const BASE_URL = import.meta.env.VITE_NEWS_API_URL;
-
+// const BASE_URL = import.meta.env.VITE_NEWS_API_URL;
+// const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+const BASE_URL = 'https://api.currentsapi.services/v1/'
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
+console.log(import.meta);
+console.log();
 
 export const getNews = async () => {
     try {
