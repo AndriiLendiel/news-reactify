060724e6ffc8bfb50d765960b7c3ead4d5dbdae9
AndriiLendiel <lendiel9333@gmail.com>
2024-11-22 14:16:47 +0200

    l1

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import Header from "./components/Header/Header"
+import NewsBanner from "./components/NewsBanner/NewsBanner"
+import Main from "./pages/Main/Main"
+
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Main />
+      </div>
+      <NewsBanner/>
+
+    </>
+  )
+}
+
+export default App
