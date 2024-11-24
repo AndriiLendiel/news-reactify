ba1b220ef770b513cbfa38a7d328f6b0ced4ef7d
AndriiLendiel <lendiel9333@gmail.com>
2024-11-24 21:25:23 +0200

    l4

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { formatDate } from "../../helpers/formatDate"
+import s from './styles.module.css'
+
+const Categories = ({ categories, setSelectedCategory, selectedCategory}) => {
+  return (
+    <div className={s.categories}>
+      {categories.map(el => {
+        return (
+          <button onClick={()=>setSelectedCategory(el)}
+            className={selectedCategory === el ? s.active : s.item}
+            key={el}
+          >{el}</button>
+        )
+      })}
+</div>
+  )
+}
+
+export default Categories
