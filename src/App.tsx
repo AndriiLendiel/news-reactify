37ccc6dd6651c2433636af79758379c3bd98ab0b
AndriiLendiel <lendiel9333@gmail.com>
2024-12-03 11:23:58 +0200

    added context & black theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-
+import React from "react"
 import Header from "./components/Header/Header"
 import Main from "./pages/Main/Main"
+import {useTheme } from "./context/ThemeContext"
+
+
+
 
 
 function App() {
-
+const {isDark, toggleTheme} = useTheme()
   return (
-    <>
+    <div className={`app ${isDark ? 'dark': 'light'}`}>
       <Header />
       <div className="container">
-        <Main />
+        <Main/>
       </div>
-
-
-    </>
+    </div>
   )
 }
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,24 @@
-import React from "react"
+import React, { useContext } from "react"
 import { formatDate } from "../../helpers/formatDate"
 import s from './styles.module.css'
+import { themeIcons } from "../../../dist/assets/theme"
+import { useTheme } from "../../context/ThemeContext"
+
+
+
+
 
 const Header = () => {
+  const {isDark, toggleTheme} = useTheme()
   return (
-      <header className={s.header}>
+    <header className={`${s.header} ${isDark ? s.dark : s.light}`}>
+      <div className={s.info}>
           <h1 className={s.title}>NEWS REACTIFY</h1>
           <p className={s.date}>{formatDate(new Date())}</p>
+      </div>
+      <img src={isDark ? themeIcons.light : themeIcons.dark}
+        width={30} alt='theme' onClick={toggleTheme} />
+
     </header>
   )
 }
